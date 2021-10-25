e7d5c5485037a742bb2f7c075160cc426a87c5d7
Viet Dinh <54ckb0y789@gmail.com>
2021-10-25 00:24:31 -0400

    Simplify code

--- packages/rust-md-core/src/events.rs
+++ packages/rust-md-core/src/events.rs
@@ -1,4 +1,4 @@
-use pulldown_cmark::{Alignment, Event, Tag};
+use pulldown_cmark::{Alignment, CodeBlockKind, Event, Tag};
 
 pub fn remap_table_headers<'a>(
     events: impl Iterator<Item = Event<'a>>,
@@ -22,11 +22,11 @@ pub fn wrap_code_block<'a>(
     events: impl Iterator<Item = Event<'a>>,
 ) -> impl Iterator<Item = Event<'a>> {
     events.scan(false, |in_pre, event| match event {
-        event @ Event::Start(Tag::CodeBlock(_)) => {
+        Event::Start(Tag::CodeBlock(_)) => {
             *in_pre = true;
             Some(event)
         }
-        event @ Event::End(Tag::CodeBlock(_)) => {
+        Event::End(Tag::CodeBlock(_)) => {
             *in_pre = false;
             Some(event)
         }
@@ -37,8 +37,8 @@ pub fn wrap_code_block<'a>(
 
 pub fn display_of(tag: &Tag) -> &'static str {
     match tag {
-        pulldown_cmark::Tag::Paragraph => "p",
-        pulldown_cmark::Tag::Heading(lvl) => match lvl {
+        Tag::Paragraph => "p",
+        Tag::Heading(lvl) => match lvl {
             1 => "h1",
             2 => "h2",
             3 => "h3",
@@ -47,30 +47,27 @@ pub fn display_of(tag: &Tag) -> &'static str {
             6 => "h6",
             _ => unreachable!(),
         },
-        pulldown_cmark::Tag::BlockQuote => "blockquote",
-        pulldown_cmark::Tag::CodeBlock(_) => "pre",
-        pulldown_cmark::Tag::List(Some(_)) => "ol",
-        pulldown_cmark::Tag::List(None) => "ul",
-        pulldown_cmark::Tag::Item => "li",
-        // pulldown_cmark::Tag::FootnoteDefinition(_) => todo!(),
-        pulldown_cmark::Tag::Table(_) => "table",
-        pulldown_cmark::Tag::TableHead => "th",
-        pulldown_cmark::Tag::TableRow => "tr",
-        pulldown_cmark::Tag::TableCell => "td",
-        pulldown_cmark::Tag::Emphasis => "em",
-        pulldown_cmark::Tag::Strong => "strong",
-        pulldown_cmark::Tag::Strikethrough => "s",
-        pulldown_cmark::Tag::Link(_, _, _) => "a",
-        pulldown_cmark::Tag::Image(_, _, _) => "img",
+        Tag::BlockQuote => "blockquote",
+        Tag::CodeBlock(_) => "pre",
+        Tag::List(Some(_)) => "ol",
+        Tag::List(None) => "ul",
+        Tag::Item => "li",
+        Tag::Table(_) => "table",
+        Tag::TableHead => "th",
+        Tag::TableRow => "tr",
+        Tag::TableCell => "td",
+        Tag::Emphasis => "em",
+        Tag::Strong => "strong",
+        Tag::Strikethrough => "s",
+        Tag::Link(..) => "a",
+        Tag::Image(..) => "img",
         _ => "span", // comment
     }
 }
 
 pub fn class_of(tag: &Tag) -> Option<String> {
     match tag {
-        pulldown_cmark::Tag::CodeBlock(pulldown_cmark::CodeBlockKind::Fenced(lang)) => {
-            Some(lang.to_string())
-        }
+        Tag::CodeBlock(CodeBlockKind::Fenced(lang)) => Some(lang.to_string()),
         _ => None,
     }
 }
@@ -82,24 +79,24 @@ pub fn attrs_of(
     (alignments, align_index): &mut AlignmentMemo,
 ) -> Option<(&'static str, String)> {
     match tag {
-        pulldown_cmark::Tag::Link(typ, dest, _title) => {
+        Tag::Link(typ, dest, _title) => {
             let href: String = match typ {
                 pulldown_cmark::LinkType::Email => format!("mailto:{}", &dest),
                 _ => dest.to_string(),
             };
             Some(("href", href))
         }
-        pulldown_cmark::Tag::TableCell | pulldown_cmark::Tag::TableHead => {
+        Tag::TableCell | Tag::TableHead => {
             let align = alignments[*align_index];
             *align_index = (*align_index + 1) % alignments.len();
             alignment_of(&align).map(|align| ("align", align.to_owned()))
         }
-        pulldown_cmark::Tag::Table(aligns) => {
+        Tag::Table(aligns) => {
             *alignments = aligns;
             *align_index = 0;
             None
         }
-        pulldown_cmark::Tag::List(Some(start)) => Some(("start", start.to_string())),
+        Tag::List(Some(start)) => Some(("start", start.to_string())),
         _ => None,
     }
 }
