e32e51078d03aab3d474efa12e4eb9d2d05b3a5e
Viet Dinh <54ckb0y789@gmail.com>
2021-11-08 01:17:47 -0500

    Upgrade flutter_math_fork

--- lib/widgets/bottom_bar.dart
+++ lib/widgets/bottom_bar.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:flutter/services.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:markdown_editor/providers.dart';
